c4ab83f8f74a7d974676ddd7170695ac8370e806
RGM-89sc <rgm89sc@gmail.com>
2020-12-15 22:39:50 +0800

    分类显示

--- src/pages/category-list/category-list.tsx
+++ src/pages/category-list/category-list.tsx
@@ -21,15 +21,46 @@ function CateoryList(props: IProps) {
     margin: 0 auto;
     padding: 1rem 1.5rem;
     width: ${contentWidth};
+
+    .category {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 20px;
+
+      .tag-name {
+        font-size: 1.5rem;
+        cursor: pointer;
+      }
+
+      .count {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        font-size: 1.5rem;
+        background-color: #eee;
+      }
+    }
   `
 
   const articlesManage = ArticlesManage.getInstance()
   const tagsMap = articlesManage.getAllTags()
-  console.log(tagsMap)
+
+  function searchArticles(tag: string) {
+    console.log(tag)
+  }
   
   return (
     <div css={categoryListCss}>
-      分类
+      {Object.keys(tagsMap).map(tag => (
+        <div className="category" key={tag}>
+          <span className="tag-name" onClick={() => searchArticles(tag)}>{tag}</span>
+          <span className="count">{tagsMap[tag].count}</span>
+        </div>
+      ))}
     </div>
   )
 }
